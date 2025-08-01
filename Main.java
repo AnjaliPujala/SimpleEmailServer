ea1bbf41b0648cda24ecf8935201869bb5a8feab
Anjali Pujala <132387262+AnjaliPujala@users.noreply.github.com>
2025-08-01 23:12:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-
+import java.net.*;
 
 class SimpleEmailServer{
 
@@ -32,9 +32,36 @@ class SimpleEmailServer{
             System.out.println("Exception while adding user: "+e);
         }
     }
-
+    private void addTextIntoFiles(BufferedWriter writer, User sender, User reciever, String subject, String body) throws Exception{
+        writer.write(sender.name);
+        writer.newLine();
+        writer.write(reciever.name);
+        writer.newLine();
+        writer.write(subject);
+        writer.newLine();
+        writer.write(body);
+        writer.newLine();
+        writer.newLine();
+    }
     public void sendEmail(User sender, User reciever, String subject, String body){
+        try{
+            Socket socket=new Socket("localhost",5000);
+            BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            addTextIntoFiles(writer,sender,reciever,subject,body);
+            writer.flush();
+            writer.close();
+            File sent=new File("users\\"+sender.name+"\\sent");
+            sent.createNewFile();
+            BufferedWriter fileWriter=new BufferedWriter(new FileWriter(sent,true));
+            addTextIntoFiles(fileWriter,sender,reciever,subject,body);
+            fileWriter.flush();
+            fileWriter.close();
 
+            socket.close();
+            System.out.println("Email sent to "+reciever+" successfully..!");
+        }catch(Exception e){
+            System.out.println("Error occured: "+e);
+        }
     }
 }
 
@@ -54,7 +81,10 @@ class User{
 public class Main{
     public static void main(String[] args){
         SimpleEmailServer emailServer=new SimpleEmailServer();
-        User user1=new User("Anjali","anjali123");
-        emailServer.registerUser(user1);
+        User sender=new User("Anjali","anjali123");
+        emailServer.registerUser(sender);
+        User receiver=new User("Nidhi","nidhi123");
+        emailServer.registerUser(receiver);
+        emailServer.sendEmail(sender, receiver, "Greetings", "Hello Nidhi, how are you?");
     }
 }
